ea9e16f981154f9c838d0b2eb5d441f227951b20
marvin <marvin@cursor.cursor.virtualbox.org>
2026-02-10 20:21:55 +0100

    updates

--- src/app/mental-arithmetic/components/arithmetic-main/mental-arithmetic-main.component.ts
+++ src/app/mental-arithmetic/components/arithmetic-main/mental-arithmetic-main.component.ts
@@ -1,9 +1,9 @@
 import {ChangeDetectionStrategy, Component} from '@angular/core';
-import {Router} from "@angular/router";
+import {Router, RouterLink} from "@angular/router";
 
 @Component({
   selector: 'app-mental-arithmetic-main',
-  imports: [],
+  imports: [RouterLink],
   templateUrl: './mental-arithmetic-main.component.html',
   styleUrl: './mental-arithmetic-main.component.css',
   changeDetection: ChangeDetectionStrategy.OnPush
